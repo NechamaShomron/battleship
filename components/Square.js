d1eba10f8170f57d52155ecfc14bf92d977b0154
Nechama Shomron <nechamasho@gmail.com>
2022-05-06 17:16:40 +0300

    Update Square.js

--- components/Square.js
+++ components/Square.js
@@ -4,7 +4,7 @@ export default function Square(props) {
   let sizeInRow = 100 / props.boardSize;
   let iPoint = props.position[0];
   let jPoint = props.position[1];
-  let squareValue = iPoint*props.boardSize+jPoint;
+  let squareValue = iPoint * props.boardSize + jPoint;
   let boardType = props.boardType;
   const handleClick = () => {
     props.onClick();
@@ -28,33 +28,34 @@ export default function Square(props) {
         {(jPoint + 9).toString(36).toUpperCase()}
       </button>
     );
-  }else if (jPoint == 0 && iPoint == 0) {
+  } else if (jPoint == 0 && iPoint == 0) {
     return (
       <button
         className="square-item place-center"
         style={{ flex: `1 0 ${sizeInRow}%` }}
-      >
-      </button>
+      ></button>
     );
   } else {
-      return (
-        <button
-          className={`square-item ${boardType == 'userBoard' &&
-                    (props.board[squareValue] == '0' && '') ||
-                    (props.board[squareValue] == 'z' && props.board[squareValue] != '0' && 'user-ship-hit ship-placed')  ||
-                    (props.board[squareValue] != '0' && 'ship-placed')
-                  }
-                    ${boardType == 'enemyBoard' &&  
-                   (props.board[squareValue] == '0' && '') ||
-                    (props.board[squareValue] == 'm' && 'miss') ||
-                    (props.board[squareValue] == 'h' && 'hit')
+    return (
+      <button
+        className={`square-item ${
+          (boardType == "userBoard" && props.board[squareValue] == "0" && "") ||
+          (props.board[squareValue] == "z" &&
+            props.board[squareValue] != "0" &&
+            "user-ship-hit ship-placed") ||
+          (props.board[squareValue] != "0" && "ship-placed")
+        }
+                    ${
+                      (boardType == "enemyBoard" &&
+                        props.board[squareValue] == "0" &&
+                        "") ||
+                      (props.board[squareValue] == "m" && "miss") ||
+                      (props.board[squareValue] == "h" && "hit")
                     }`}
-          style={{ flex: `1 0 ${sizeInRow}%`}}
-          value={props.board[squareValue]}
-          onClick={handleClick}
-        >
-        </button>
-      );
-   
+        style={{ flex: `1 0 ${sizeInRow}%` }}
+        value={props.board[squareValue]}
+        onClick={handleClick}
+      ></button>
+    );
   }
 }
